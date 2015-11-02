fd1caa91a030d484f93b3bd5fc7a6f02484fe536
ddekany <ddekany@apache.org>
2015-11-02 20:06:54 +0100

    Adding some of the freemarker.org content (until it's back online).

---
+++ toc.js
@@ -0,0 +1,180 @@
+
+var toc = {
+"title": "FreeMarker",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "What is FreeMarker?",
+"url": "index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Download / Maven",
+"url": "freemarkerdownload.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Documentation",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Manual",
+"url": "docs/index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Java API",
+"url": "docs/api/index.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Manual Chinese\n translation",
+"url": "https://sourceforge.net/projects/freemarker/files/chinese-manual/",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Tooling",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Editor / IDE plugins",
+"url": "editors.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Online template\n tester",
+"url": "http://freemarker-online.kenshoo.com/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "File generator tool\n (FMPP)",
+"url": "http://fmpp.sourceforge.net/",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Community",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Report bugs",
+"url": "https://issues.apache.org/jira/browse/FREEMARKER/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Bugs still in the old\n tracker",
+"url": "https://sourceforge.net/p/freemarker/bugs/new/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Ask help on Stack\n Overflow",
+"url": "http://stackoverflow.com/questions/ask?tags=freemarker",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Get news on Twitter",
+"url": "https://twitter.com/freemarker",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Discuss on mailing lists",
+"url": "mailing-lists.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Who uses FreeMarker?",
+"url": "poweredBy.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Contributors wanted!",
+"url": "contribute.html",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+,
+{
+"title": "Miscellaneous",
+"url": null,
+"isFile": true,
+"children": [
+{
+"title": "Sourceforge.net project\n page",
+"url": "https://sourceforge.net/projects/freemarker/",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Source code (Git)",
+"url": "sourcecode.html",
+"isFile": true,
+"children": [
+]
+}
+,
+{
+"title": "Project history",
+"url": "history.html",
+"isFile": true,
+"children": [
+]
+}
+]
+}
+]
+}
+;
